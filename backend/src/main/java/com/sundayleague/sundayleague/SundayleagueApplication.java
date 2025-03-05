dbda08c7b27fcdc5811eab41c4c13920a701bf75
goodjunseon <pzs2001926@gmail.com>
2025-03-05 20:45:05 +0900

    first commit of Backend

---
+++ backend/src/main/java/com/sundayleague/sundayleague/SundayleagueApplication.java
@@ -0,0 +1,13 @@
+package com.sundayleague.sundayleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundayleagueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sundayleague/sundayleague/SundayleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundayleague.sundayleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundayleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
